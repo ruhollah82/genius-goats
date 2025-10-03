92b4e10e8a1349d4a5b3582d58f7c84676e7b991
efatemee <fatemeesm001@gmail.com>
2025-10-03 14:39:29 +0330

    neetcode add solution new have input output

--- Valid Anagram/ruhi.py
+++ Valid Anagram/ruhi.py
@@ -18,3 +18,30 @@ class Solution:
                 dT[i] =1            
 
         return dS == dT
+
+
+#solution fateme 1
+
+s=input()
+t=input()
+dic1={}
+dic2={}
+for i in s:
+    if i in dic1:
+        dic1[i]+=1
+    else:
+        dic1[i]=1
+    
+for i in t:
+    if i in dic2:
+        dic2[i]+=1
+    else:
+        dic2[i]=1
+
+print(dic1==dic2)
+
+
+
+
+#solution important
+print(sorted(s)==sorted(t))
